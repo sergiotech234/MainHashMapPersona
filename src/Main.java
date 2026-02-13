e688eb62afece36bcc5af4ed2df03d65d0f5a178
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2026-02-13 10:57:08 +0100

    Ejercicio 4, BC3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Map;
+import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Creación de un HashMap con claves tipo String y valores tipo Integer
+        Map<String, Integer> edades = new HashMap<>();
+        // Agregar pares clave-valor
+        edades.put("Ana", 20);
+        edades.put("Luis", 22);
+        edades.put("María", 25);
+        // Acceso por clave
+        System.out.println("Edad de Ana: " + edades.get("Ana"));
+        // Eliminar un par clave-valor
+        edades.remove("Luis");
+        // Recorrer el mapa mostrando claves y valores
+        for (String nombre : edades.keySet()) {
+            System.out.println(nombre + ": " + edades.get(nombre));
         }
     }
 }
